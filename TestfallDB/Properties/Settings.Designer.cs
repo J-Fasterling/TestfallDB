48dc4ba9426e94903ade84fd108ec663f2f672d4
Jonas Fasterling <67583771+J-Fasterling@users.noreply.github.com>
2021-04-11 19:43:29 +0200

    lokale Datenbank erstellt

--- TestfallDB/Properties/Settings.Designer.cs
+++ TestfallDB/Properties/Settings.Designer.cs
@@ -32,5 +32,16 @@ namespace TestfallDB.Properties {
                 return ((string)(this["ConnectionString"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\TestfallDB.md" +
+            "f;Integrated Security=True;Connect Timeout=30")]
+        public string TestfallDBConnectionString {
+            get {
+                return ((string)(this["TestfallDBConnectionString"]));
+            }
+        }
     }
 }
