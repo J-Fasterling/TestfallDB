f6df9d650fe5535a7170ebb6966902c7a2f7d49d
Jonas Fasterling <67583771+J-Fasterling@users.noreply.github.com>
2021-04-12 14:00:35 +0200

    SQL-Edit Klasse

---
+++ TestfallDB/SQL-Edit.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace TestfallDB
+{
+    class SQL_Edit
+    {
+        public static SqlConnection connection = null;
+        public static string sqlString = "";
+
+        public void SqlConnect(string sqlString)
+        {
+            connection = new SqlConnection(sqlString);
+
+            connection.Open();
+        }
+
+
+        public void SqlAdd(string tableName, string column, string toAdd)
+        {
+            SqlCommand command = new SqlCommand();
+            command.Connection = SQL_Edit.connection;
+            command.CommandText = "insert into " + tableName + " values (@" + column + ")";
+            command.Parameters.AddWithValue(column, toAdd);
+            command.ExecuteNonQuery();
+        }
+    }
+}
